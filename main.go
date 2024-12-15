d590212ecc85b82acc9f507f8858c010eafeac79
maria.tellez <maria.tellez@sezzle.com>
2024-12-15 12:24:57 -0500

    adding mod

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 	"net/http"
 )
+const portNumber = ":8080"
+//If it starts with uppercase the function is public, else is private 
+func Home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "This is the home page")
+}
 
+func About(w http.ResponseWriter, r *http.Request){
+	sum := addValues(2,2)
+	_,_=fmt.Fprint(w, fmt.Sprintf("This is the home page %d", sum))
+}
+
+func addValues(x, y int) int{
+	return x + y
+}
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprint(w, "Hello, World")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes: %d", n))
-	})
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
